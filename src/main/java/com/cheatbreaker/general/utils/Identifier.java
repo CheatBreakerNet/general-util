81edff598d886dba4b7117cf74b1a1aad0f8f451
lowercasebtw <jejojora2@gmail.com>
2025-11-08 20:30:44 -0500

    Add parseOrNull

--- src/main/java/com/cheatbreaker/general/utils/Identifier.java
+++ src/main/java/com/cheatbreaker/general/utils/Identifier.java
@@ -2,13 +2,14 @@ package com.cheatbreaker.general.utils;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.jspecify.annotations.Nullable;
 
 import java.util.regex.Pattern;
 
 @AllArgsConstructor
 @Getter
 public class Identifier {
-    private static final Pattern PATTERN = Pattern.compile("");
+    private static final Pattern PATTERN = Pattern.compile("[a-z0-9_.-]");
 
     private final String namespace;
     private final String path;
@@ -34,6 +35,14 @@ public class Identifier {
         }
 
         return new Identifier(namespace, path);
+    }
+
+    public static @Nullable Identifier parseOrNull(String input) {
+        try {
+            return parse(input);
+        } catch (InvalidException exception) {
+            return null;
+        }
     }
 
     public static class InvalidException extends Exception {
