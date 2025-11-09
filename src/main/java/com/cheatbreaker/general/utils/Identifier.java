99226d4f7bc547deccc14cc71cc42ca9c2da533a
lowercasebtw <jejojora2@gmail.com>
2025-11-09 15:46:30 -0500

    Rename Identifier#parse* to Identifier#of* and add safety constructor

--- src/main/java/com/cheatbreaker/general/utils/Identifier.java
+++ src/main/java/com/cheatbreaker/general/utils/Identifier.java
@@ -1,21 +1,27 @@
 package com.cheatbreaker.general.utils;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.jspecify.annotations.Nullable;
 
 import java.util.regex.Pattern;
 
-@AllArgsConstructor
 @Getter
 public class Identifier {
     private static final Pattern NAMESPACE_PATTERN = Pattern.compile("[a-z0-9_.-]+");
-    private static final Pattern PATH_PATTERN = Pattern.compile("[A-Za-z0-9_/.-]+");
+    private static final Pattern PATH_PATTERN = Pattern.compile("[A-Za-z0-9/._-]+");
 
     private final String namespace;
     private final String path;
 
-    public static Identifier parse(String input) throws InvalidException {
+    private Identifier(String namespace, String path) {
+        assert isValidNamespace(namespace);
+        this.namespace = namespace;
+
+        assert isValidPath(path);
+        this.path = path;
+    }
+
+    public static Identifier of(String input) throws InvalidException {
         if (input == null) {
             throw new InvalidException("Invalid identifier input, input is null!");
         } else {
@@ -31,20 +37,24 @@ public class Identifier {
             }
 
             if (!isValidNamespace(namespace)) {
-                throw new InvalidException("Invalid identifier \"" + input + "\", namespace only accepts " + NAMESPACE_PATTERN.pattern());
+                throw new InvalidException("Invalid identifier namespace \"" + input + "\", only accepts " + NAMESPACE_PATTERN.pattern());
             }
 
             if (!isValidPath(path)) {
-                throw new InvalidException("Invalid identifier \"" + input + "\", path only accepts " + PATH_PATTERN.pattern());
+                throw new InvalidException("Invalid identifier path \"" + input + "\", only accepts " + PATH_PATTERN.pattern());
             }
 
             return new Identifier(namespace, path);
         }
     }
 
-    public static @Nullable Identifier parseOrNull(String input) {
+    public static Identifier of(String namespace, String path) {
+        return new Identifier(namespace, path);
+    }
+
+    public static @Nullable Identifier ofNullable(String input) {
         try {
-            return parse(input);
+            return of(input);
         } catch (InvalidException exception) {
             System.out.println("Failed to parse identifier! " + exception);
             exception.printStackTrace();
