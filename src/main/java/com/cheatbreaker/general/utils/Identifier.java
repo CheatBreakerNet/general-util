f09f460af5bfa6345f2230a9683ad15316cc22da
lowercasebtw <jejojora2@gmail.com>
2025-11-09 00:41:37 -0500

    Fix Identifier parsing & Add validation for namespace/path separately

--- src/main/java/com/cheatbreaker/general/utils/Identifier.java
+++ src/main/java/com/cheatbreaker/general/utils/Identifier.java
@@ -9,7 +9,8 @@ import java.util.regex.Pattern;
 @AllArgsConstructor
 @Getter
 public class Identifier {
-    private static final Pattern PATTERN = Pattern.compile("[a-z0-9_.-]");
+    private static final Pattern NAMESPACE_PATTERN = Pattern.compile("[a-z0-9_.-]+");
+    private static final Pattern PATH_PATTERN = Pattern.compile("[A-Za-z0-9_/.-]+");
 
     private final String namespace;
     private final String path;
@@ -17,32 +18,46 @@ public class Identifier {
     public static Identifier parse(String input) throws InvalidException {
         if (input == null) {
             throw new InvalidException("Invalid identifier input, input is null!");
-        }
-
-        if (!PATTERN.matcher(input).matches()) {
-            throw new InvalidException("Invalid identifier input, identifier only accepts " + PATTERN.pattern());
-        }
-
-        String namespace = "minecraft";
-        String path;
-
-        String[] parts = input.split(":");
-        if (parts.length == 0) {
-            path = input;
         } else {
-            namespace = parts[0];
-            path = input.substring(namespace.length());
-        }
+            String namespace = "minecraft";
+            String path;
 
-        return new Identifier(namespace, path);
+            String[] parts = input.split(":", 2);
+            if (parts.length == 2) {
+                namespace = parts[0];
+                path = parts[1];
+            } else {
+                path = parts[0];
+            }
+
+            if (!isValidNamespace(namespace)) {
+                throw new InvalidException("Invalid identifier namespace \"" + input + "\", namespace only accepts " + NAMESPACE_PATTERN.pattern());
+            }
+
+            if (!isValidPath(path)) {
+                throw new InvalidException("Invalid identifier path \"" + input + "\", path only accepts " + PATH_PATTERN.pattern());
+            }
+
+            return new Identifier(namespace, path);
+        }
     }
 
     public static @Nullable Identifier parseOrNull(String input) {
         try {
             return parse(input);
         } catch (InvalidException exception) {
+            System.out.println("Failed to parse identifier! " + exception);
+            exception.printStackTrace();
             return null;
         }
+    }
+
+    public static boolean isValidNamespace(String namespace) {
+        return namespace != null && NAMESPACE_PATTERN.matcher(namespace).matches();
+    }
+
+    public static boolean isValidPath(String path) {
+        return path != null && PATH_PATTERN.matcher(path).matches();
     }
 
     @Override
